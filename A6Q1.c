27a0223ac06e1aeb9c7987055401c71abd5bd53a
AbstractDil <nandysagar@yahoo.com>
2022-09-05 22:44:21 +0530

    diff --git a/A6Q1.c b/A6Q1.c

--- A6Q1.c
+++ A6Q1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 
-    int a,b,c;
+    int a,b,c,d,e;
 
     printf("Enter a number: ");
     scanf("%d",&a);
@@ -27,6 +27,42 @@ printf("The value of c is %d \n ",c);
     }
 
     printf("The value of b is %d \n ",b);
+
+    // using switch case
+
+    switch(a){
+        case 1:
+            printf("The value of d is 2 \n");
+            break;
+        case 2:
+            printf("The value of d is 3 \n");
+            break;
+        case 4:
+            printf("The value of d is 6 \n");
+            break;
+        default:
+            printf("The value of d is 0 \n");
+            break;
+    }
+
+    // Alternative way using switch case
+
+    switch(a){
+        case 1:
+            e=2;
+            break;
+        case 2:
+            e=3;
+            break;
+        case 4:
+            e=6;
+            break;
+        default:
+            e=0;
+            break;
+    }
+
+    printf("The value of e is %d \n",e);
 
     return 0;
 }
